02e98322b61125690afccb75267f8b2427e20844
Patrick Navega <psnavega97@icloud.com>
2023-03-05 20:03:51 -0300

    feat: remove dotenv config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import { connectToMongoDb } from "./configs/mongo";
 import { connectToRedis } from "./configs/redis";
